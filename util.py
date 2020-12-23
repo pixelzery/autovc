dab2e9599d1d6382c6d3567d57b9d21e1a490b6b
pixelzery <pixelzery@gmail.com>
2020-12-23 16:03:47 +0000

    update util.py downloading from drive

--- util.py
+++ util.py
@@ -4,23 +4,44 @@ import shutil
 import requests
 import sys
 import re
+
 try:
     from google.colab import drive
 except:
     pass
 
-def download_file(url, file_name):
-    # adapted from https://stackoverflow.com/a/39217788/5013267
-    with requests.get(url, stream=True) as r:
-        with open(file_name, 'wb') as f:
-            r.raw.read = functools.partial(r.raw.read, decode_content=True)
-            shutil.copyfileobj(r.raw, f)
 
-def download_from_drive_id(drive_id, file_name):
-    pre_url = "https://drive.google.com/u/0"
-    html = requests.get(pre_url + "/uc?id={}&export=download".format(drive_id)).text
-    confirm_code = re.search(r'href="\/uc\?export=download&amp;confirm=(.+?)&amp;', html).group(1)
-    download_file(pre_url + "/uc?export=download&confirm={}&id={}".format(confirm_code, drive_id), file_name)
+# https://stackoverflow.com/a/39225272/5013267
+def download_from_drive_id(drive_id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': drive_id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': drive_id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
 
 
 # the following code is from https://github.com/minimaxir/gpt-2-simple/blob/master/gpt_2_simple/gpt_2.py
@@ -31,15 +52,18 @@ def mount_gdrive():
 
     drive.mount('/content/drive')
 
+
 def is_mounted():
     """Checks if the Google Drive is mounted."""
     assert os.path.isdir('/content/drive'), "You must mount first using mount_gdrive()"
+
 
 def copy_file_to_gdrive(file_path):
     """Copies a file to a mounted Google Drive."""
     is_mounted()
 
     shutil.copyfile(file_path, "/content/drive/My Drive/" + file_path)
+
 
 def copy_file_from_gdrive(file_path):
     """Copies a file from a mounted Google Drive."""
